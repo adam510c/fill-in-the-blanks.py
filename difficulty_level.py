9422385fa8f8dca2144836e5f96b3dcf7a0e1453
adam510c <adamboydfrancis@gmail.com>
2016-03-31 18:22:21 -0600

    very close to functional
    Need to fix punctuation error in list

--- difficulty_level.py
+++ difficulty_level.py
@@ -10,7 +10,7 @@ def difficulty_level():
 #allows user to select difficulty level easy, medium, hard
     difficulty = ""
     while (difficulty != "easy") or (difficulty != "medium") or (difficulty != "hard"):
-        difficulty = raw_input("Select your difficulty level: easy, medium or hard.")
+        difficulty = raw_input("Select your difficulty level: easy, medium or hard. ")
         difficulty = difficulty.lower()
         if difficulty == "easy":
             print "You chose easy."
@@ -22,12 +22,35 @@ def difficulty_level():
             print "You chose hard."
             return difficulty
         else:
-            print "Please select easy, medium or hard."
+            print "Please select easy, medium or hard. "
     return quiz_text
+
+def guesses():
+    while True:
+        try:
+            number_of_guesses = int(raw_input('How many guesses would you like'
+            ' for each question? '))
+        except ValueError:
+            print "Please enter a positive integer."
+            continue
+        if number_of_guesses < 1:
+            print "Please enter a positive integer."
+            continue
+        else:
+            print 'You will get ' + str(number_of_guesses) + ' guesses per question.'
+        return number_of_guesses
+
+def blank_finder(quiz_string):
+    for word in quiz_string:
+        if "__" not in word:
+            return word
+        else:
+            None
 
 def play_game():
     quiz_text = []
     quiz_key = []
+
     difficulty = difficulty_level()
     if difficulty == "easy":
         quiz_text = easy_quiz
@@ -38,6 +61,34 @@ def play_game():
     if difficulty == "hard":
         quiz_text = hard_quiz
         quiz_key = hard_key
+
+    attempts = guesses()
     print quiz_text
+    question_index = 0
+    quiz_string = quiz_text.split()
+
+    while (question_index < len(quiz_key)) and attempts > 0:
+        answer = raw_input('What word belongs in blank number '
+        + str(question_index + 1) + '? ')
+        if attempts == 0:
+            print 'You are out of guesses. Your game is over.'
+            return
+        if answer == quiz_key[question_index]:
+            print "Correct!"
+            print quiz_string
+            replace = quiz_string.index('__' + str(question_index + 1) + '__') #fix puctuation error
+            quiz_string[replace] = quiz_key[question_index]
+            amended_text = " ".join(quiz_string)
+            print amended_text
+            question_index += 1
+        else:
+            print "Wrong!"
+            attempts = attempts - 1
+            if attempts == 1:
+                print "It's now or never Sport. This is your last chance."
+            if attempts == 0:
+                print 'Peace out hombre. Your time here is done.'
+
+
 
 play_game()
